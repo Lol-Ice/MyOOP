32af34489ffa6ba8525f4f96d2a472b5096a0599
Lol-ice <lolice@mail.ru>
2022-06-22 15:41:30 +0300

    2 commit

--- src/test/kotlin/service/WallServiceTest.kt
+++ src/test/kotlin/service/WallServiceTest.kt
@@ -10,9 +10,13 @@ class WallServiceTest {
     @Test
     fun addTest() {
         val posts = WallService
-        val result = posts.add(Post(1, 1, 1, 1, "22.05.2022", "1",1, 1, true ,1,1,1,
-            1, 1, "1", 1, true, 1, null, true,
-            true, true, true, true, true, 1 ))
+        val result = posts.add(
+            Post(
+                1, 1, 1, 1, "22.05.2022", "1", 1, 1, true, 1, 1, 1,
+                1, 1, "1", 1, true, 1, null, true,
+                true, true, true, true, true, 1
+            )
+        )
 
         assertNotEquals(0, result.id)
     }
@@ -20,9 +24,11 @@ class WallServiceTest {
     @Test
     fun updateTestTrue() {
         val posts = WallService
-        val update = Post(1, 1, 1, 1, "22.05.2022", "1",1, 1, true ,1,1,1,
+        val update = Post(
+            1, 1, 1, 1, "22.05.2022", "1", 1, 1, true, 1, 1, 1,
             1, 1, "1", 1, true, 1, null, true,
-            true, true, true, true, true, 1 )
+            true, true, true, true, true, 1
+        )
         val result = posts.update(update)
         assertTrue(result)
     }
@@ -30,9 +36,11 @@ class WallServiceTest {
     @Test
     fun updateTestFalse() {
         val posts = WallService
-        val update = Post(0, 1, 1, 1, "22.05.2022", "1",1, 1, true ,1,1,1,
+        val update = Post(
+            0, 1, 1, 1, "22.05.2022", "1", 1, 1, true, 1, 1, 1,
             1, 1, "1", 1, true, 1, null, true,
-            true, true, true, true, true, 1 )
+            true, true, true, true, true, 1
+        )
         val result = posts.update(update)
         assertFalse(result)
     }
@@ -40,9 +48,11 @@ class WallServiceTest {
     @Test
     fun historyPostTestNoHistory() {
         val posts = WallService
-        val historyPost = Post(1, 1, 1, 1, "22.05.2022", "1",1, 1, true ,1,1,1,
+        val historyPost = Post(
+            1, 1, 1, 1, "22.05.2022", "1", 1, 1, true, 1, 1, 1,
             1, 1, "1", 1, true, 1, null, true,
-            true, true, true, true, true, 1 )
+            true, true, true, true, true, 1
+        )
         val result = posts.historyPost(historyPost)
         assertNotEquals("Нет истории", result)
     }
