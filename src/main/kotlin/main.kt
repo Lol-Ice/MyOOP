76c4cb1d12a7cc0c9d5e6a765e9f08791bc91698
Lol-ice <lolice@mail.ru>
2022-06-25 13:30:02 +0300

    1.2 commit

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -2,6 +2,7 @@ package data.service
 
 import data.*
 import data.service.service.WallService
+
 fun main() {
 
     var arrayOfAttachment = emptyArray<Attachment>()
@@ -12,9 +13,13 @@ fun main() {
     arrayOfAttachment += NoteAttachment("note")
     arrayOfAttachment += PageAttachment("page")
 
-    val post1 = WallService.add(Post(1, 1, 1, 1, "22.05.2022", "1",1, 1, true ,1,1,1,
-        1, 1, "1", 1, true, 1, null, true,
-        true, true, true, true, true, 1, arrayOfAttachment))
+    val post1 = WallService.add(
+        Post(
+            1, 1, 1, 1, "22.05.2022", "1", 1, 1, true, 1, 1, 1,
+            1, 1, "1", 1, true, 1, null, true,
+            true, true, true, true, true, 1, arrayOfAttachment
+        )
+    )
 
     print(post1)
 
