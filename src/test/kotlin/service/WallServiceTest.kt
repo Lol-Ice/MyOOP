f4af213cb19b2e65dd7bad50168a606e5f6496b0
Lol-ice <lolice@mail.ru>
2022-06-25 13:06:53 +0300

    1.1 commit

--- src/test/kotlin/service/WallServiceTest.kt
+++ src/test/kotlin/service/WallServiceTest.kt
@@ -2,60 +2,49 @@ import org.junit.Test
 
 import org.junit.Assert.*
 
-import data.service.service.WallService
+import data.service.service.*
 import data.*
 
 class WallServiceTest {
 
+    private val posts = WallService
+    private val arrayOfAttachment = emptyArray<Attachment>()
     @Test
     fun addTest() {
-        val posts = WallService
-        val result = posts.add(
-            Post(
-                1, 1, 1, 1, "22.05.2022", "1", 1, 1, true, 1, 1, 1,
-                1, 1, "1", 1, true, 1, null, true,
-                true, true, true, true, true, 1
-            )
-        )
+
+        val result = posts.add(Post(1, 1, 1, 1, "22.05.2022", "1",1, 1, true ,1,1,1,
+            1, 1, "1", 1, true, 1, null, true,
+            true, true, true, true, true, 1, arrayOfAttachment))
 
         assertNotEquals(0, result.id)
     }
-
     @Test
     fun updateTestTrue() {
-        val posts = WallService
-        val update = Post(
-            1, 1, 1, 1, "22.05.2022", "1", 1, 1, true, 1, 1, 1,
+
+        val result = posts.update(Post(1, 1, 1, 1, "22.05.2022", "1",1, 1, true ,1,1,1,
             1, 1, "1", 1, true, 1, null, true,
-            true, true, true, true, true, 1
-        )
-        val result = posts.update(update)
+            true, true, true, true, true, 1, arrayOfAttachment))
+
         assertTrue(result)
     }
-
     @Test
     fun updateTestFalse() {
-        val posts = WallService
-        val update = Post(
-            0, 1, 1, 1, "22.05.2022", "1", 1, 1, true, 1, 1, 1,
+
+        val result = posts.update(Post(0, 1, 1, 1, "22.05.2022", "1",1, 1, true ,1,1,1,
             1, 1, "1", 1, true, 1, null, true,
-            true, true, true, true, true, 1
-        )
-        val result = posts.update(update)
+            true, true, true, true, true, 1, arrayOfAttachment))
+
         assertFalse(result)
     }
 
     @Test
     fun historyPostTestNoHistory() {
-        val posts = WallService
-        val historyPost = Post(
-            1, 1, 1, 1, "22.05.2022", "1", 1, 1, true, 1, 1, 1,
+
+        val result = posts.historyPost(Post(1, 1, 1, 1, "22.05.2022", "1",1, 1, true ,1,1,1,
             1, 1, "1", 1, true, 1, null, true,
-            true, true, true, true, true, 1
-        )
-        val result = posts.historyPost(historyPost)
+            true, true, true, true, true, 1, arrayOfAttachment))
+
         assertNotEquals("Нет истории", result)
     }
-
 
 }
